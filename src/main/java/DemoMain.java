7c4b4f90ef66e47ded548dd8bc6df6bf300e1c1d
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-05-31 00:43:41 +0200

    better comparison diplayed. stackoverflow related question.

--- src/main/java/DemoMain.java
+++ src/main/java/DemoMain.java
@@ -6,8 +6,10 @@ import com.google.common.collect.Range;
 public class DemoMain {
 
     public static void main(String[] args) {
-        // write your code here
+        String url = "https://stackoverflow.com/questions/16711147/populating-a-list-with-a-contiguous-range-of-integers";
+        System.out.println(url);
         System.out.println("python3: print(list(range(1,101)))");
+        System.out.println("guava: ContiguousSet.create(Range.closed(1, 100), DiscreteDomain.integers()).asList())");
         System.out.println(ContiguousSet.create(Range.closed(1, 100), DiscreteDomain.integers()).asList());
     }
 }
